192cfcaf5214f988f5c3d78b866e1f77c8520bc4
satyadeep-neupane <satyadeep.neupane@deerwalk.edu.np>
2022-11-27 16:23:22 +0545

    ENV Added

--- app/routes/route.auth.js
+++ app/routes/route.auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const authController = require('../controller/authController');
 
-router.post('login', authController.attemptLogin);
+router.post('/login', authController.attemptLogin);
 router.post('refresh', authController.getNewAccessToken);
 
 module.exports = router;
